44f1f1fa299edc0fac898e293ec7d37c379c3652
lisen <1311532900@qq.com>
2020-07-21 01:04:55 +0800

    add Login.vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router.js'
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+import axios from 'axios'
+//请求接口的基本路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios;
 
 Vue.config.productionTip = false
 
+Vue.use(ElementUI);//全局导入element-ui
+
 new Vue({
+  router,//挂载路由
   render: h => h(App),
 }).$mount('#app')
