153651e5fc5b98ebad12940a912bf449dcfaf523
lisen <1311532900@qq.com>
2020-07-22 21:25:38 +0800

    complete login ,add Home.vue

---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        Home组件
+        <div>
+            <el-button type="info" @click="loginOut" :plain="true">退出</el-button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        methods: {
+            loginOut() {
+                sessionStorage.clear()
+                this.$router.replace('/login')
+                this.$message({
+                    message: '退出成功！',
+                    type: 'success'
+                });
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './components/Login'
+import Home from "./components/Home";
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
     routes: [
         {
             path: '/',
@@ -13,6 +14,19 @@ export default new Router({
         {
             path: '/login',
             component: Login
+        },
+        {
+            path: '/home',
+            component: Home
         }
     ]
 })
+
+router.beforeEach((to, from, next) => {
+    if (to.path === '/login') return next();
+    const tokenStr = sessionStorage.getItem('token')
+    if (!tokenStr) return next('/login');
+    else next()
+})
+
+export default router
